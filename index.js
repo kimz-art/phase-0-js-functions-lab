b66f6a54414d2bc514117cd1e713dd9aa67f41a0
Samuel Mwaura <mwaurakimemia08@gmail.com>
2026-04-03 15:15:04 +0300

    update js file

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
+function calculateTax(amount){
+    return amount*=0.1;
+}
 
+function convertToUpperCase(text){
+    return text.toUpperCase();
+}
 
+function findMaximum(num1, num2){
+    if (num1>num2){
+        return num1;
+    }        
+    else{return num2;}
+}
 
+function isPalindrome(word){
+    let reversed=word.split("").reverse().join("");
+    return word===reversed;
+}
 
+function calculateDiscountedPrice(originalPrice, discountedPercantage){
+    let discount = discountedPercantage/100 *originalPrice
+    return originalPrice - discount;
+}
+
+console.log(calculateTax(100));
+console.log(convertToUpperCase("hello"));
+console.log(findMaximum(10,15));
+console.log(isPalindrome("madam"));
+console.log(calculateDiscountedPrice(100,20));
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
